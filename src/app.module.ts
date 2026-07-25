558eb9e6f6aa9fec9a7b269452d23811f835a909
MurhafMoussa <murhaf.moussa@hotmail.com>
2026-07-25 22:00:54 +0300

    feat: integrate i18n support with nestjs-i18n and add user registration DTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import * as path from 'path';
+import { AuthModule } from './modules/auth/auth.module';
 
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, './locales/'),
+        watch: true,
+      },
+      resolvers: [
+        new QueryResolver(['lang']),
+        AcceptLanguageResolver,
+      ],
+      typesOutputPath: path.join(__dirname, '../src/generated/i18n.generated.ts'),
+    }), AuthModule,],
 })
-export class AppModule {}
+export class AppModule { }
 
