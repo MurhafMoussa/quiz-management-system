84f51839353abdea3556dbc8aa39564b95bee50b
MurhafMoussa <murhaf.moussa@hotmail.com>
2026-07-25 20:36:51 +0300

    feat: create UserRepository interface for user data management

---
+++ src/auth/domain/interfaces/user-repository.ts
@@ -0,0 +1,6 @@
+import { User } from "../entities/user.entity";
+
+export abstract class UserRepository {
+    abstract findById(id: string): Promise<User | null>;
+    abstract save(user: User): Promise<void>;
+}
