a8ccc1b8cc02cbd9b6bade8d39aad5be2ea3ef67
MurhafMoussa <murhaf.moussa@hotmail.com>
2026-08-04 19:52:08 +0300

    refactor(database): update user schema and migration for first and last name

---
+++ prisma/migrations/20260803134017_update_user_schema_with_roles_and_first_last_names/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `username` on the `users` table. All the data in the column will be lost.
+  - Added the required column `first_name` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `last_name` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'STUDENT', 'TEACHER');
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "username",
+ADD COLUMN     "first_name" TEXT NOT NULL,
+ADD COLUMN     "last_name" TEXT NOT NULL,
+ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'STUDENT';
